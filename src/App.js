4b532e457f60ef1309ddc672544741c5fd090610
Andrii Rastorhuiev <efinswim@gmail.com>
2022-04-19 14:28:01 +0300

    initial project, added two components (ClassCounter, Counter)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+
+function App() {
+
+  return (
+    <div>
+      {/*<Counter />*/}
+      <ClassCounter />
+    </div>
+  );
+}
+
+export default App;
